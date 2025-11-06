f48cf17aa53a668384246eb33941871cda8b3663
07-mvieira <177216400+07-mvieira@users.noreply.github.com>
2025-11-06 10:51:05 -0300

    protótipo da avaliação

---
+++ form/form.js
@@ -0,0 +1,15 @@
+const piiContainer = document.getElementById("pii");
+
+const listaEscolas =
+    [
+        "Escola Municipal Lorem",
+        "Escola Ipsum",
+        "Escola São Dolor",
+        "Escola Sit",
+        "Escola Amet"
+    ];
+
+// let questionZeroValue = document.getElementById("question-0").value;
+
+// let piiName = document.getElementById("")
+
