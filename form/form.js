023d87ef66ab65ffe7ca8407fc0b96e6ab130899
07-mvieira <177216400+07-mvieira@users.noreply.github.com>
2025-11-07 10:26:07 -0300

    pequenas funcionalidades

--- form/form.js
+++ form/form.js
@@ -1,15 +1,32 @@
-const piiContainer = document.getElementById("pii");
+const piiForm = document.getElementById('pii');
+const piiSubmit = document.getElementById("pii-submit");
+const mainForm = document.getElementById('main-form');
+const questions = document.querySelectorAll('.question');
+let answeredQuestions = 0;
 
-const listaEscolas =
-    [
-        "Escola Municipal Lorem",
-        "Escola Ipsum",
-        "Escola São Dolor",
-        "Escola Sit",
-        "Escola Amet"
-    ];
+piiForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-// let questionZeroValue = document.getElementById("question-0").value;
+    piiForm.style.display = "none";
+    mainForm.style.display = "flex";
+});
 
-// let piiName = document.getElementById("")
+questions.forEach(question => {
+    const ratingSliders = question.querySelectorAll('.rating');
+
+    ratingSliders.forEach(ratingSlider => {
+        ratingSlider.addEventListener('input', () => {
+            const test = question.querySelector('.test')
+            test.innerHTML = ratingSlider.value
+        })
+    })
+
+    const clearStars = question.querySelector('.clear-stars');
+
+    const clearAnswer = question.querySelector('.clear-answer')
+});
+
+// let questionZeroValue = document.getElementById('question-0').value;
+
+// let piiName = document.getElementById('')
 
